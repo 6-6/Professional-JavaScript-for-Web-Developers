db8e4b5925ec29710df070dcfdb681d18fe5b80a
6-6 <jing2215@foxmail.com>
2022-01-04 11:31:31 +0800

    备份

--- chapter22/22.4/EventTarget.js
+++ chapter22/22.4/EventTarget.js
@@ -3,7 +3,6 @@ function EventTarget(){
 }
 
 EventTarget.prototype = {
-    constructor: EventTarget,
     /* 
     绑定一个自定义事件，本质意义上是将事件处理程序，形成队列放入handlers对象中type数组
     @params {String} type 自定义事件类型名称
@@ -18,30 +17,42 @@ EventTarget.prototype = {
     },
     /* 
     触发事件
-    @params {Object} event 例如：{ type: 'message', message: 'Hello world!' }
+    @params {Object} event 事件对象 类似原生的event事件
+    > @params {String} type 事件类型，例如原生click事件
+    > @params {String} message 事件消息
     */
     fire: function(event){
-        console.log(event);
+        //为什么要将this赋值在target属性中？因为这里模拟的是原生事件对象event.target
         if(!event.target){
             event.target = this;
         }
 
+        //通过当前函数传的event.type匹配this.handlers[]，是否存在绑定的事件
         if(this.handlers[event.type] instanceof Array){
             var handlers = this.handlers[event.type];
             for(var i=0, len=handlers.length; i<len; i++){
+                //调用事件处理函数
                 handlers[i](event);
             }
         }
     },
+    /* 
+    移除事件
+    @params {String} type 自定义事件类型名称
+    @params {Function} handler 自定义事件处理方法
+    */
     removeHandler: function(type, handler){
+        //判断是否有要移除的事件
         if(this.handlers[type] instanceof Array){
             var handlers = this.handlers[type];
             for(var i=0, len=handlers.length; i<len; i++){
+                //存在相等的事件处理函数，break的意义在于保持i的值
                 if(handlers[i] === handler){
                     break;
                 }
             }
 
+            //移除handlers某个对象
             handlers.splice(i, 1);
         }
     }
